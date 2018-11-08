5f55501e55f09e14d10c3963b926744a0f9244e6
yuktakodwani <kodwani.yukta@gmail.com>
2018-11-08 18:23:53 -0500

    adding changes

---
+++ Comic-books-gallery/Controllers/ComicBookscontroller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic_books_gallery.Controllers
+{
+    public class ComicBookscontroller : Controller
+    {
+
+        public ActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "  The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p> Final issue! Witness the final hour </ p >";
+            ViewBag.Artists = new string[]
+            {
+                "Script: dan scott",
+            "Pencils: humberto ramos ",
+            "Inks: victor olazaba",
+            "Colors: edger delgado",
+            "Letters: chris eliopoulos"
+            };
+
+            return View();
+
+        }
+    }
+}
