cc02b70fceaad628be6944a8558445cf8d879288
yuktakodwani <kodwani.yukta@gmail.com>
2018-11-08 19:04:25 -0500

    added changes

---
+++ Comic-books-gallery/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic_books_gallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+       
+
+    }
+}
